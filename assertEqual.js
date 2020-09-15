eae2626f1b3a5e8f66d1232f0f208b4a33eb2886
Al Lotina <lotina.al@gmail.com>
2020-09-15 01:00:12 +0000

    assertEqual.js added

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('😁Assertion Passed: ' + actual + ' === ' + expected);
+  } else if (actual !== expected) { 
+    console.log('🙁Assertion Failed: ' + actual + ' !== ' + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
