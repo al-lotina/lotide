7cdc90f09b115ecefc861b757b7b6aed6b474da2
Al Lotina <lotina.al@gmail.com>
2020-09-16 03:08:48 +0000

    eqArrays.js added

---
+++ eqArrays.js
@@ -0,0 +1,26 @@
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😁Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) { 
+    console.log(`🙁Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+function eqArrays (arr1, arr2) {
+  if (arr1.length === arr2.length){ 
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+  }return true;
+}
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
