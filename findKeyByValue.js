fda760e43c854cc3f2b4abdc43049a658ca8d446
Al Lotina <lotina.al@gmail.com>
2020-09-17 02:29:32 +0000

    findKeyByValue.js added

---
+++ findKeyByValue.js
@@ -0,0 +1,18 @@
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+function findKeyByValue (obj, val) {
+  for (let key in obj) {
+    if(obj[key] === val) {
+      return key;
+    }
+  }
+}
+
+console.log(findKeyByValue(bestTVShowsByGenre, "The Wire")); // drama
+console.log(findKeyByValue(bestTVShowsByGenre, "That '70s Show")); // undefined
+console.log(findKeyByValue(bestTVShowsByGenre, "The Expanse")); // sci_fi
+
