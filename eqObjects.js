c964d37e076889f35edfe7c47c39bc1d8291ac10
Al Lotina <lotina.al@gmail.com>
2020-09-17 23:13:02 +0000

    map.js added

--- eqObjects.js
+++ eqObjects.js
@@ -21,16 +21,3 @@ const eqObjects = function(object1, object2) {
 };
 
 console.log(eqObjects(bestTVShowsByGenre, copyOfBestTVShows));
-/*
-function eqArrays (arr1, arr2) {
-  if (arr1.length === arr2.length){ 
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      } 
-    } 
-    return true;
-  } 
-  return false;
-}
-*/
