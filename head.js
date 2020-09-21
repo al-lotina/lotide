88bfe72c0ac2d5dd1ea73b512d8f95aad1005775
Al Lotina <lotina.al@gmail.com>
2020-09-21 16:50:55 +0000

    assertEqualTest.js and headTest.js added

--- head.js
+++ head.js
@@ -1,15 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`😁Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) { 
-    console.log(`🙁Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 const head = function(arr) {
   let firstEl = arr[0];
   return firstEl;
 };
 
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
