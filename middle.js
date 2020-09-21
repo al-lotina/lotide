44f65787e4288eeae8e02d25d372f0bbb4d281c3
Al Lotina <lotina.al@gmail.com>
2020-09-21 23:05:39 +0000

    talTest.js and middleTest.js refactored with Mocha and Chai

--- middle.js
+++ middle.js
@@ -1,23 +1,4 @@
-/*
-function eqArrays (arr1, arr2) {
-  if (arr1.length === arr2.length){ 
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      } 
-    } 
-    return true;
-  } 
-  return false;
-}
-
-const assertArraysEqual = function(actual, expected) {
-  eqArrays(actual, expected) 
-   ? console.log(`😁Assertion Passed: ${actual} === ${expected}`) 
-   : console.log(`🙁Assertion Failed: ${actual} !== ${expected}`);      
-}
-*/
-function middle (arr) {
+const middle = function (arr) {
   let newArr = [];
   if (arr.length === 1 || arr.length === 2) {
     newArr = [] 
@@ -29,16 +10,9 @@ function middle (arr) {
     newArr.push(arr[arr.length/2 - 1]);
     newArr.push(arr[arr.length/2]);
   }
-  console.log(newArr);
-}  
+  return newArr;
+};
 
 module.exports = middle;
 
-// middle([1]) // => []
-// middle([1, 2]) // => []
 
-// middle([1, 2, 3]) // => [2]
-// middle([1, 2, 3, 4, 5]) // => [3]
-
-// middle([1, 2, 3, 4]) // => [2, 3]
-// middle([1, 2, 3, 4, 5, 6, 3, 8]) // => [4, 5]
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,16 @@
-const assertArraysEqual = require('../assertEqual');
+// const assertArraysEqual = require('../assertEqual');
 const tail = require('../tail');
 
-console.log(tail(["Yo Yo", "Lighthouse", "Labs"]));
-assertArraysEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+const assert = require('chai').assert;
+
+// console.log(tail(["Yo Yo", "Lighthouse", "Labs"]));
+// assertArraysEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+
+describe("#tail", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  it("returns [] for ['5']", () => {
+    assert.deepEqual(tail(['5']), []); 
+  });  
+});
