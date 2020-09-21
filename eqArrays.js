1a1ce1464007c52efe2643f4104992701a35d27d
Al Lotina <lotina.al@gmail.com>
2020-09-21 20:19:06 +0000

    eqArrays.js refactored and eqArraysTest.js added

--- eqArrays.js
+++ eqArrays.js
@@ -12,10 +12,6 @@ const eqArrays = function (arr1, arr2) {
 
 module.exports = eqArrays;
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
 
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
 
 
