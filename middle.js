8a26bac90eff4eface3e407f37484a91de8e0f84
Al Lotina <lotina.al@gmail.com>
2020-09-21 21:57:21 +0000

    middle.js refactored and middleTest.js added

--- middle.js
+++ middle.js
@@ -32,11 +32,13 @@ function middle (arr) {
   console.log(newArr);
 }  
 
-middle([1]) // => []
-middle([1, 2]) // => []
+module.exports = middle;
 
-middle([1, 2, 3]) // => [2]
-middle([1, 2, 3, 4, 5]) // => [3]
+// middle([1]) // => []
+// middle([1, 2]) // => []
 
-middle([1, 2, 3, 4]) // => [2, 3]
-middle([1, 2, 3, 4, 5, 6, 3, 8]) // => [4, 5]
+// middle([1, 2, 3]) // => [2]
+// middle([1, 2, 3, 4, 5]) // => [3]
+
+// middle([1, 2, 3, 4]) // => [2, 3]
+// middle([1, 2, 3, 4, 5, 6, 3, 8]) // => [4, 5]
